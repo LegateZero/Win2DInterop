935ee1f58667796f78e296fe6298f9e214858937
LegateZero <defender834@mgmail.com>
2024-12-13 07:33:46 -0800

    Added default projects

---
+++ Win2DInterop/InteropHelper.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "InteropHelper.h"
+#include "InteropHelper.g.cpp"
+
+namespace winrt::Win2DInterop::implementation
+{
+    int32_t InteropHelper::MyProperty()
+    {
+        return 10;
+    }
+
+    void InteropHelper::MyProperty(int32_t val)
+    {
+       
+    }
+}
---
+++ Win2DInterop/InteropHelper.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "InteropHelper.g.h"
+
+namespace winrt::Win2DInterop::implementation
+{
+    struct InteropHelper : InteropHelperT<InteropHelper>
+    {
+        InteropHelper() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::Win2DInterop::factory_implementation
+{
+    struct InteropHelper :InteropHelperT<InteropHelper, implementation::InteropHelper>
+    {
+    };
+}
